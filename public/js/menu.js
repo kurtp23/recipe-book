24cce6addf56c864f3c9e90ed9998f35180a1649
kurtp23 <kurt_phillips@live.com>
2021-02-24 19:06:40 -0800

    working route structure/working handlebars

--- public/js/menu.js
+++ public/js/menu.js
@@ -5,7 +5,8 @@ const view = document.getElementById("view");
 view.addEventListener("click", (event) => {
   event.preventDefault();
   console.log("this is the button");
-  fetch("/view", { method: "GET" });
+  //   fetch("/view", { method: "GET" });
+  window.location.replace("/view");
 });
 
 // view.addEventListener("click", (event) => {
--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-const express = require("express");
-const PORT = process.env.PORT || 3000;
-const app = express();
+var express = require("express");
 
-// Initialize body parser
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Define static file
-app.use(express.static("public"));
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-require("./routes/html-routes.js")(app);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.listen(PORT, () => {
-  console.log(`Listening on PORT ${PORT}`);
+// Import routes and give the server access to them.
+var routes = require("./routes/html-routes.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+  console.log("App now listening at localhost:" + PORT);
 });

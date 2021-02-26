4e89920e53cfa07620230af56596bcaf6c10621c
Ray Luna <leon.luna.ray@gmail.com>
2021-02-25 18:15:35 -0800

    testing sequelize connection

--- models/index.js
+++ models/index.js
@@ -1,42 +1,20 @@
-const fs = require('fs');
-const path = require('path');
 const Sequelize = require('sequelize');
 
-const basename = path.basename(module.filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/db.js`)[env];
-const db = {};
-let sequelize;
-
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
-
-fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
-      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
-    );
-  })
-  .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+const db = new Sequelize('recipe_book_db', 'postgres', process.env.DB_PASSWORD, {
+  host: 'localhost',
+  dialect: postgres,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
   }
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+//test db
+db.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });

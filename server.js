48140d8a96e6c9d0e176fe4c73ecd2e2ba570f9a
Ray Luna <leon.luna.ray@gmail.com>
2021-02-25 19:59:15 -0800

    table working

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require("express");
+const express = require("express");
 const db = require("./models");
 
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-var app = express();
+const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -13,13 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./routes/html-routes.js");
+const routes = require("./routes/html-routes.js");
 
 app.use(routes);
 

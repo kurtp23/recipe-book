07a7d7aa9e3a264a147ebf52efc22a40e58e53e0
Ray Luna <leon.luna.ray@gmail.com>
2021-02-25 17:43:25 -0800

    update sequelize

--- models/index.js
+++ models/index.js
@@ -1,13 +1,42 @@
+const fs = require('fs');
+const path = require('path');
 const Sequelize = require('sequelize');
-const db = require('../config/db.js');
 
-const recipes = db.define('recipes', {
-  title: {
-    type: Sequelize.STRING
+const basename = path.basename(module.filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(`${__dirname}/../config/db.js`)[env];
+const db = {};
+let sequelize;
+
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    );
+  })
+  .forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
   }
 });
 
-recipes.sync().then(() => {
-  console.log('table created');
-});
-module.exports = recipes;
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;

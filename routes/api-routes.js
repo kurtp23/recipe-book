326c1a2159d7e0092c9a1eff038e9d672625d82d
kurtp23 <kurt_phillips@live.com>
2021-02-26 17:58:18 -0800

    partial get api route for recipes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -16,9 +16,9 @@ router.post("/api/addRecipe", (req, res) => {
   console.log(req.body);
   res.status(200).end();
 });
-router.post("/api/login", (req, res) => {
-  console.log(req.body);
-});
+// router.post("/api/login", (req, res) => {
+//   console.log(req.body);
+// });
 router.post("/api/newUser", (req, res) => {
   console.log(req.body);
 });

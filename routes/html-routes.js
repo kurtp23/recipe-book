ddc0e5ebdd388ff2c07cbb6a4109df6ae0f75670
Alexander Fok <foktmalexander@gmail.com>
2021-02-26 22:30:14 -0800

    Redirect to /login for homepage if token is not present

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const authenticateToken = require("../auth/middleware/authenticateToken");
 const router = express.Router();
 
 router.get("/view", (req, res) => res.render("recipes"));
 router.get("/", (req, res) => {
-  res.render("menu", {});
+  const auth = req.headers.authorization;
+  const token = auth && auth.split(" ")[1];
+  if (!token) return res.redirect("/login");
+
+  jwt.verify(token, privateKey, (err, user) => {
+    if (err) return res.status(401).end();
+    res.render("menu", {});
+  });  
 });
 router.get("/login", (req, res) => {
   res.render("login", {});
@@ -14,7 +22,7 @@ router.get("/signUp", (req, res) => {
 router.get("/newRecipe", (req, res) => {
   res.render("add", {});
 });
-router.get("/testAuth", (req, res) => {
+router.get("/testAuth", authenticateToken, (req, res) => {
   res.render("testAuth", {});
 });
 module.exports = router;

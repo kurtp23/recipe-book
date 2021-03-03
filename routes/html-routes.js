4fcc6ff54b25c222c5cf932623dbbdcb12a1c59e
Ray Luna <leon.luna.ray@gmail.com>
2021-03-02 17:32:53 -0800

    cleaned up code/eslint rules

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,10 @@
-const express = require("express");
-const authenticateToken = require("../auth/middleware/authenticateToken");
-const router = express.Router();
-const db = require("../models");
+const express = require('express');
+const authenticateToken = require('../auth/middleware/authenticateToken');
+const db = require('../models');
 
-router.get("/view", (req, res) => {
+const router = express.Router();
+
+router.get('/view', (req, res) => {
   // db.RecIng.findAll({
   //   where: {
   //     recId: recipe.id,
@@ -20,37 +21,37 @@ router.get("/view", (req, res) => {
     console.log(JSON.parse(ingredients[1]));
     // db.Ingredient.get();
     const json = {
-      recipes: [{ name: "q" }, { name: "w" }, { name: "e" }],
+      recipes: [{ name: 'q' }, { name: 'w' }, { name: 'e' }],
       recipe: {
-        name: "something",
+        name: 'something',
         instructions: JSON.parse(instructions[2]),
         ingredients: JSON.parse(ingredients[2]),
       },
     };
 
-    res.render("recipes", json);
+    res.render('recipes', json);
   });
 });
-router.get("/", authenticateToken, (req, res) => {
-  res.render("menu", {});
+router.get('/', authenticateToken, (req, res) => {
+  res.render('menu', {});
 });
-router.get("/login", (req, res) => {
-  res.render("login", {});
+router.get('/login', (req, res) => {
+  res.render('login', {});
 });
-router.get("/signUp", (req, res) => {
-  res.render("newUser", {});
+router.get('/signUp', (req, res) => {
+  res.render('newUser', {});
 });
-router.get("/newRecipe", (req, res) => {
-  res.render("add", {});
+router.get('/newRecipe', (req, res) => {
+  res.render('add', {});
 });
-router.get("/testAuth", authenticateToken, (req, res) => {
-  res.render("testAuth", {});
+router.get('/testAuth', authenticateToken, (req, res) => {
+  res.render('testAuth', {});
 });
-router.get("/test", (req, res) => {
-  res.render("test", {});
+router.get('/test', (req, res) => {
+  res.render('test', {});
 });
-router.get("/authenticate", (req, res) => {
-  res.render("authenticate", {});
+router.get('/authenticate', (req, res) => {
+  res.render('authenticate', {});
 });
 
 module.exports = router;

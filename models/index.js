80164da6074ba09a0d2ce899cae68a3d1a74b554
Alexander Fok <foktmalexander@gmail.com>
2021-03-04 19:41:13 -0800

    Fix Syntax Error

--- models/index.js
+++ models/index.js
@@ -10,7 +10,7 @@ const Sequelize = require('sequelize');
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = process.env.JAWSDB_URL ? {...process.env.JAWSDB_URL, "dialect": "mysql"} : require(__dirname + '/../config/config.json')[env];
+const config = process.env.JAWSDB_URL ? {...process.env.JAWSDB_URL, dialect: "mysql"} : require(__dirname + '/../config/config.json')[env];
 
 const db = {};
 

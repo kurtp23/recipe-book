4889e7115cde6ae9910a49cbd051551dff53b007
Ray Luna <leon.luna.ray@gmail.com>
2021-03-05 20:30:07 -0800

    add print functionality

--- public/js/recipes.js
+++ public/js/recipes.js
@@ -16,3 +16,14 @@ searchBtn.addEventListener('click', (event) => {
   const searchString = document.getElementById('search').value;
   window.location.replace(`/search/${searchString}`);
 });
+
+function printDiv(divName) {
+  const printContents = document.getElementById(divName).innerHTML;
+  const originalContents = document.body.innerHTML;
+
+  document.body.innerHTML = printContents;
+
+  window.print();
+
+  document.body.innerHTML = originalContents;
+}
